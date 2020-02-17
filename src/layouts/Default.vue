9125f9399735d6df7702bcd27114cd09506209b6
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-17 15:40:15 +0100

    Add static page of reference detail

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,6 +7,7 @@
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/reference-detail/">Detail reference</g-link>
       </nav>
     </header>
     <slot/>
@@ -22,25 +23,10 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
   height: 80px;
 }
 
---
+++ src/pages/ReferenceDetail.vue
@@ -0,0 +1,136 @@
+<template>
+    <Layout>
+        <div class="ml-auto mr-auto max-w-screen-2xl">
+            <section class="flex flex-wrap pt-16 pb-20">
+                <div class="w-full px-4 lg:w-1/2 lg:pr-12">
+                    <h1 class="mb-6 text-3xl font-extrabold leading-none">Optimalizace procesu vypracování smluv</h1>
+                    <a href="#nogo" class="inline-block mb-5 text-green-500">M&M reality a.s.</a>
+                    <div class="mb-6 text-base wysiwyg-content">Prodej či nákup nemovitosti je záležitost, kterou je třeba bezchybně smluvně ošetřit. V rámci služby realitní makléři poskytují svým klientům právní servis (ve spolupráci s právním oddělením).</div>
+                </div>
+                <div class="w-full px-4 lg:w-1/2">
+                    <img src="https://barth-net.cz/data/vehicle/car/photos/image_size_1024_768_inset_75/skoda-fabia-combi-style-1-0tsi-81kw5e455107e561c.JPG"
+                         alt="">
+                </div>
+            </section>
+        </div>
+
+        <div class="py-8">
+            <div class="ml-auto mr-auto max-w-screen-2xl">
+                <section class="flex flex-wrap">
+                    <div class="self-center w-full px-4 lg:w-1/2 lg:pr-12">
+                        <h2 class="mb-8 text-2xl font-bold">Výzva</h2>
+                        <div class="text-base wysiwyg-content">
+                            <p>
+                                Majitelé nemovitostí chtějí od realitních makléřů špičkovou službu - nemovitost na prohlídce
+                                dobře odprezentovat a předat zájemcům všechny potřebné informace, aby se mohli rozhodnout.
+                                Ti nejlepší makléři začali proto zájemcům předávat tištěný katalog s technickými parametry,
+                                fotkami, listem vlastnicví, vzorem rezervační a kupní smlouvy atd. Vytvořit takový katalog
+                                ručně znamená min. 2 hodiny práce na sběr dat a grafickou sazbu.
+                                Obchodní manažeři u nás tedy poptali nástroj, díky kterému by byli schopni makléři takový
+                                profesionální katalog udělat rychle a jednoduše, a díky tomu ho mohli mít u všech svých
+                                nemovitostí.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="self-start w-full px-4 lg:w-1/2 group">
+                        <!--TODO Create component-->
+                        <div class="bg-green-200 rounded group-hover:shadow-xl">
+                            <div class="pt-16 mx-16">
+                                <img src="https://www.mmreality.cz/data/imgs/logo.png" alt="logo partnera" class="mb-8">
+                                <div class="wysiwyg-content">
+                                    <p>
+                                    M&M reality je největší a neúspěšnější česká realitní kancelář. Ročně zodbchoduje
+                                    přes 30 000 nemovitostí a věnuje také developerské a investiční činnosti.
+                                    Klienty obsluhuje 188 poboček a 2 500 makléřů. Opakovaně držitel ceny “Firma roku”
+                                    od Hospodářské komory ČR.</p>
+                                </div>
+                            </div>
+                            <a href="#nogo" class="inline-flex items-center py-2 pl-16 mb-16 text-xs font-bold bg-gray-300 rounded-r-full group-hover:text-white group-hover:bg-gradient-r-blue-green">
+                                Co k zakázce řekl klient?
+                                <span class="px-4">chevron-right</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        </div>
+
+        <div class="ml-auto mr-auto max-w-screen-2xl">
+            <section class="flex flex-wrap">
+                <div class="self-center w-full px-4 lg:w-1/2 lg:pr-12">
+                    <h2 class="mb-8 text-2xl font-bold">Řešení</h2>
+                    <div class="text-base wysiwyg-content">
+                        <p class="">Vytvořili jsme nástroj, díky kterému můžu makléř vytvořit profesionální katalog skutečně rychle - do
+                            10min. Předvyplníme mu popis a tech. parametry nemovitosti, doplníme list vlastnictví z katastru nemovitostí, přidáme představení makléře podle jeho skutečných referencí a doplníme cross-selling služby. Makléři mají možnost si katalog různě upravovat:
+                        </p>
+                        <ul class="">
+                            <li>otáčet a ořezávat fotky přidávat další stránky z nabídky nebo vlastní PDF soubory, které systém umístí do stránky tak, aby byly vizuálně součástí katalogu měnit pořadí stránek, upravit textové popisy získat náhled výsledného katalogu vytisknout nebo sdílet přes URL-shortener odkaz a další…</li>
+                            <li>Da</li>
+                        </ul>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet debitis ut vel. Cupiditate, eos eum ex, iusto libero necessitatibus, obcaecati officia possimus quis recusandae rerum sed! Accusamus dolore illo perferendis.</p>
+                    </div>
+                </div>
+                <div class="self-start w-full px-4 lg:w-1/2">
+                    <div class="ml-16">
+                        <h3 class="mb-8 text-xl font-bold">Dodané služby</h3>
+                        <ul class="mb-16">
+                            <li>Projektové řízení</li>
+                            <li>UX/UI design</li>
+                            <li>Vývoj back-end</li>
+                        </ul>
+                        <h3 class="mb-8 text-xl font-bold">Technologie</h3>
+
+                        <div class="lg:w-1/2">
+                            <div class="grid grid-cols-4 gap-2">
+                                <div class="flex items-center justify-center p-2 border border-gray-300">
+                                    <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
+                                </div>
+                                <div class="flex items-center justify-center p-2 border border-gray-300">
+                                    <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
+                                </div>
+                                <div class="flex items-center justify-center p-2 border border-gray-300">
+                                    <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
+                                </div>
+                                <div class="flex items-center justify-center p-2 border border-gray-300">
+                                    <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
+                                </div>
+                                <div class="flex items-center justify-center p-2 border border-gray-300">
+                                    <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="ml-auto mr-auto max-w-screen-2xl">
+            <section class="flex flex-wrap pb-16">
+                <div class="relative w-full px-4 lg:w-1/2 lg:pr-12">
+                    <h2 class="mb-8 text-2xl font-bold">Výsledek</h2>
+                    <div class="mb-6 text-base wysiwyg-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus et eveniet necessitatibus nisi pariatur, porro quae quia quis recusandae repellendus sequi tenetur ut vero? Animi, dolores, hic. Iusto, omnis, quibusdam?</p>
+                    </div>
+                    <div class="absolute inset-y-0 right-0 w-1 h-full rounded bg-gradient-t-blue-green"></div>
+                </div>
+                <div class="w-full px-4 lg:w-1/2 lg:pl-12">
+                    <h2 class="mb-8 text-2xl font-bold text-green-500">Výsledek</h2>
+                    <div class="text-lg wysiwyg-content">
+                        <p>
+                            “We enjoy working with Spiria because they have proven to be experienced, professional and reliable. Since 2004, Spiria has helped us with a variety of complex projects and they continue to be a valued partner for expert development services.”
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </Layout>
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: 'Reference'
+    }
+  }
+</script>
