c08c38c6f1fba4afe52189bd286faf78b2cc1b07
Jakub Robotka <jakub.robotka@gmail.com>
2020-02-18 14:15:06 +0100

    Chore: Update tailwind config colors, grid and aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        'sans': [
+          'Muli',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       colors: {
+        gray: {
+          100: "#E9E9E9",
+          200: "#d5d5d5",
+          300: "#c1c1c1",
+          400: "#adadad",
+          500: "#999999",
+          600: "#858585",
+          700: "#717171",
+          800: "#5d5d5d",
+          900: "#494949"
+        },
         blue: {
           100: "#50cdff",
           200: "#3cb9ff",
@@ -24,19 +53,96 @@ module.exports = {
           800: "#368308",
           900: "#226f00"
         },
+        gray: {
+          300: '#f2f2f2',
+        },
+      },
+      linearGradientDirections: {
+        't': 'to top',
+        'tr': 'to top right',
+        'r': 'to right',
+        'br': 'to bottom right',
+        'b': 'to bottom',
+        'bl': 'to bottom left',
+        'l': 'to left',
+        'tl': 'to top left',
+      },
+      linearGradientColors: theme => ({
+        'blue-green': [theme('colors.blue.500') , theme('colors.green.500')],
+      }),
+      fontSize: {
+        xs: ".875rem",
+        sm: "1rem",
+        base: "1.125rem",
+        lg: "1.25rem",
+        xl: "1.625rem",
+        "2xl": "2.375rem",
+        "3xl": "3.75rem"
+      },
 
-        fontSize: {
-          "2xs": ".875rem",
-          xs: "1rem",
-          sm: "1.125rem",
-          base: "1.25rem",
-          lg: "1.625rem",
-          xl: "2.375rem",
-          "2xl": "3.75rem"
-        }
+      width: theme => ({
+        auto: 'auto',
+        ...theme('spacing'),
+        '1/24': '4.16667%',
+        '2/24': '8.33333%',
+        '3/24': '12.5%',
+        '4/24': '16.66667%',
+        '5/24': '20.83333%',
+        '6/24': '25%',
+        '7/24': '29.16667%',
+        '8/24': '33.33333%',
+        '9/24': '37.5%',
+        '10/24': '41.66667%',
+        '11/24': '45.83333%',
+        '12/24': '50%',
+        '13/24': '54.16667%',
+        '14/24': '58.33333%',
+        '15/24': '62.5%',
+        '16/24': '66.66667%',
+        '17/24': '70.83333%',
+        '18/24': '75%',
+        '19/24': '79.16667%',
+        '20/24': '83.33333%',
+        '21/24': '87.5%',
+        '22/24': '91.66667%',
+        '23/24': '95.83333%',
+        full: '100%',
+        screen: '100vw',
+      }),
+
+      maxWidth: {
+        "screen-2xl": "1400px",
+      },
+      screens: {
+        xs: '360px',
+        sm: `640px`,
+        md: `768px`,
+        lg: `1024px`,
+        xl: `1280px`,
+      },
+
+      borderWidth: {
+        '3': '3px'
+      },
+
+      aspectRatio: {
+        'none': 0,
+        'square': [1, 1],
+        '16/9': [16, 9],
+        '4/3': [4, 3],
+        '21/9': [21, 9],
       }
     }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    linearGradients: ['responsive', 'hover', 'focus', 'group-hover'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    translate: ['responsive', 'hover', 'focus', 'group-hover'],
+    aspectRatio: ['responsive'],
+  },
+  plugins: [
+    require('tailwindcss-gradients'),
+    require('tailwindcss-aspect-ratio'),
+  ]
 };
