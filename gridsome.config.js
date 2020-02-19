587061ba766337bb625e612dde871ac67a3d2d79
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-19 12:00:58 +0100

    Add svg sprite builder - not working

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,8 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
+const merge = require('webpack-merge')
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const tailwind = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss");
 const autoprefixer = require("autoprefixer");
@@ -15,6 +16,26 @@ if (process.env.NODE_ENV === "production") {
   postcssPlugins.push(autoprefixer);
 }
 
+const projectConfig =  {
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          spriteFilename: '~/assets/sprite.svg',
+          esModule: false,
+        }
+      },
+    ]
+  },
+  plugins: [
+    new SpriteLoaderPlugin({
+      plainSprite: true,
+    }),
+  ]
+};
 module.exports = {
   siteName: "Digital Solutions",
   plugins: [],
@@ -24,5 +45,8 @@ module.exports = {
         plugins: postcssPlugins
       }
     }
+  },
+  configureWebpack(config) {
+    return merge(projectConfig , config)
   }
 };
