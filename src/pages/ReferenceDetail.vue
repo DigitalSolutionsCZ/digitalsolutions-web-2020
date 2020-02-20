d5a450c36ac2828901b730ac286bba6006daea59
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-20 12:02:13 +0100

    Unify layout of reference detail

--- src/pages/ReferenceDetail.vue
+++ src/pages/ReferenceDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout>
         <div class="ml-auto mr-auto max-w-screen-2xl">
-            <section class="flex flex-wrap pt-16 pb-20">
+            <section class="flex flex-wrap pt-16 pb-16">
                 <div class="w-full px-4 lg:w-1/2 lg:pr-12">
                     <h1 class="mb-6 text-3xl font-extrabold leading-none">Optimalizace procesu vypracování smluv</h1>
                     <a href="#nogo" class="inline-block mb-5 text-green-500">M&M reality a.s.</a>
@@ -14,10 +14,11 @@
             </section>
         </div>
 
-        <div class="py-8">
-            <div class="ml-auto mr-auto max-w-screen-2xl">
+        <div class="relative mb-16">
+            <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full" fit="cover"/>
+            <div class="relative py-8 ml-auto mr-auto max-w-screen-2xl">
                 <section class="flex flex-wrap">
-                    <div class="self-center w-full px-4 lg:w-1/2 lg:pr-12">
+                    <div class="self-center w-full px-4 lg:w-14/24 lg:pr-12">
                         <h2 class="mb-8 text-2xl font-bold">Výzva</h2>
                         <div class="text-base wysiwyg-content">
                             <p>
@@ -32,9 +33,9 @@
                             </p>
                         </div>
                     </div>
-                    <div class="self-start w-full px-4 lg:w-1/2 group">
+                    <div class="self-start w-full px-4 lg:w-10/24 group">
                         <!--TODO Create component-->
-                        <div class="bg-green-200 rounded group-hover:shadow-xl">
+                        <div class="bg-white rounded group-hover:shadow-xl">
                             <div class="pt-16 mx-16">
                                 <img src="https://www.mmreality.cz/data/imgs/logo.png" alt="logo partnera" class="mb-8">
                                 <div class="wysiwyg-content">
@@ -45,9 +46,8 @@
                                     od Hospodářské komory ČR.</p>
                                 </div>
                             </div>
-                            <a href="#nogo" class="inline-flex items-center py-2 pl-16 mb-16 text-xs font-bold bg-gray-300 rounded-r-full group-hover:text-white group-hover:bg-gradient-r-blue-green">
+                            <a href="#client-survey" class="inline-flex items-center py-2 pl-16 mb-16 text-xs font-bold bg-gray-300 rounded-r-full group-hover:text-white group-hover:bg-gradient-r-blue-green">
                                 Co k zakázce řekl klient?
-                                <span class="px-4">chevron-right</span>
                             </a>
                         </div>
 
@@ -55,10 +55,9 @@
                 </section>
             </div>
         </div>
-
-        <div class="ml-auto mr-auto max-w-screen-2xl">
-            <section class="flex flex-wrap">
-                <div class="self-center w-full px-4 lg:w-1/2 lg:pr-12">
+        <div class="relative ml-auto mr-auto max-w-screen-2xl">
+            <section class="flex flex-wrap mb-8">
+                <div class="self-center w-full px-4 lg:w-14/24 lg:pr-12">
                     <h2 class="mb-8 text-2xl font-bold">Řešení</h2>
                     <div class="text-base wysiwyg-content">
                         <p class="">Vytvořili jsme nástroj, díky kterému můžu makléř vytvořit profesionální katalog skutečně rychle - do
@@ -71,18 +70,36 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet debitis ut vel. Cupiditate, eos eum ex, iusto libero necessitatibus, obcaecati officia possimus quis recusandae rerum sed! Accusamus dolore illo perferendis.</p>
                     </div>
                 </div>
-                <div class="self-start w-full px-4 lg:w-1/2">
-                    <div class="ml-16">
+                <div class="self-start w-full px-4 lg:w-10/24">
+                    <div class="mb-8 ml-16">
                         <h3 class="mb-8 text-xl font-bold">Dodané služby</h3>
                         <ul class="mb-16">
-                            <li>Projektové řízení</li>
-                            <li>UX/UI design</li>
-                            <li>Vývoj back-end</li>
+                            <li class="flex items-center mb-3">
+                                <icon
+                                    symbol="chevron"
+                                    class="flex-shrink-0 w-3 h-3 mr-2 text-green-500 fill-current">
+                                </icon>
+                                <span class="text-lg truncate">Bakendarna</span>
+                            </li>
+                            <li class="flex items-center mb-3">
+                                <icon
+                                        symbol="chevron"
+                                        class="flex-shrink-0 w-3 h-3 mr-2 text-green-500 fill-current">
+                                </icon>
+                                <span class="text-lg truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur blanditiis culpa dicta distinctio doloribus, earum eligendi error hic incidunt minima nihil odit officiis qui quod recusandae sit voluptate!</span>
+                            </li>
+                            <li class="flex items-center">
+                                <icon
+                                        symbol="chevron"
+                                        class="flex-shrink-0 w-3 h-3 mr-2 text-green-500 fill-current">
+                                </icon>
+                                <span class="text-lg truncate">Bakendarna</span>
+                            </li>
                         </ul>
                         <h3 class="mb-8 text-xl font-bold">Technologie</h3>
 
-                        <div class="lg:w-1/2">
-                            <div class="grid grid-cols-4 gap-2">
+                        <div class="lg:w-20/24">
+                            <div class="grid grid-cols-4 gap-4">
                                 <div class="flex items-center justify-center p-2 border border-gray-300">
                                     <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
                                 </div>
@@ -136,36 +153,71 @@
                         </div>
                     </div>
                     <template #prevArrow>
-                        <div class="absolute inset-y-0 left-0 flex items-center -ml-10">
-                            <
+                        <div class="absolute inset-y-0 left-0 flex items-center -ml-10 cursor-pointer">
+                            <icon
+                                    symbol="chevron_thin_stroke"
+                                    class="w-8 h-8 fill-current transform rotate-180"
+                            ></icon>
                         </div>
                     </template>
                     <template #nextArrow>
-                        <div class="absolute inset-y-0 right-0 flex items-center -mr-10">
-                            >
+                        <div class="absolute inset-y-0 right-0 flex items-center -mr-10 cursor-pointer">
+                            <icon
+                                    symbol="chevron_thin_stroke"
+                                    class="w-8 h-8 fill-current"
+                            ></icon>
                         </div>
                     </template>
                 </VueSlickCarousel>
             </div>
         </div>
         <div class="ml-auto mr-auto max-w-screen-2xl">
-            <section class="flex flex-wrap pb-16">
-                <div class="relative w-full px-4 lg:w-1/2 lg:pr-12">
+            <section class="flex flex-wrap pb-16" >
+                <div class="relative w-full px-4 lg:w-1/2 lg:pr-16">
                     <h2 class="mb-8 text-2xl font-bold">Výsledek</h2>
                     <div class="mb-6 text-base wysiwyg-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus et eveniet necessitatibus nisi pariatur, porro quae quia quis recusandae repellendus sequi tenetur ut vero? Animi, dolores, hic. Iusto, omnis, quibusdam?</p>
                     </div>
                     <div class="absolute inset-y-0 right-0 w-1 h-full rounded bg-gradient-t-blue-green"></div>
                 </div>
-                <div class="w-full px-4 lg:w-1/2 lg:pl-12">
-                    <h2 class="mb-8 text-2xl font-bold text-green-500">Výsledek</h2>
-                    <div class="text-lg wysiwyg-content">
-                        <p>
-                            “We enjoy working with Spiria because they have proven to be experienced, professional and reliable. Since 2004, Spiria has helped us with a variety of complex projects and they continue to be a valued partner for expert development services.”
-                        </p>
+                <div class="w-full px-4 lg:w-1/2 lg:pl-16" id="client-survey">
+                    <div class="lg:w-18/24">
+                        <h2 class="mb-8 text-2xl font-bold text-green-500">Vyjádření klienta</h2>
+                        <div class="text-lg wysiwyg-content">
+                            <p>
+                                “We enjoy working with Spiria because they have proven to be experienced, professional and reliable. Since 2004, Spiria has helped us with a variety of complex projects and they continue to be a valued partner for expert development services.”
+                            </p>
+                        </div>
+                        <div class="flex items-center">
+                            <img class="flex-shrink-0 w-24 h-24 mr-4 rounded-full" src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="avatar">
+                            <div>
+                                <strong class="text-lg">Zdeněk samek</strong>
+                                <div class="text-base">regionální ředitel pro Prahu a , M&M reality</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
+        </div>
+        <div class="bg-gradient-r-blue-green">
+            <div class="ml-auto mr-auto max-w-screen-2xl">
+                <div class="flex">
+                    <div class="w-1/2">
+                        <a href="#nogo" class="inline-flex items-center py-6 text-xl text-white">
+                            <icon symbol="chevron" class="w-3 h-3 mr-2 fill-current transform rotate-180"></icon>
+                            Zpět na seznam referencí
+                        </a>
+                    </div>
+                    <div class="w-1/2">
+                        <div class="text-right">
+                            <a href="#nogo" class="inline-flex items-center py-6 text-xl text-white">
+                                Další reference
+                                <icon symbol="chevron" class="w-3 h-3 ml-2 fill-current"></icon>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </Layout>
 </template>
