7c6d918f911d8b3e4e9cb60b43c67b8b41cd29e3
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-25 14:27:16 +0100

    Add responsive for reference detail

--- src/pages/ReferenceDetail.vue
+++ src/pages/ReferenceDetail.vue
@@ -1,11 +1,11 @@
 <template>
     <Layout>
         <div class="ml-auto mr-auto max-w-screen-xl">
-            <section class="flex flex-wrap pt-16 pb-16">
+            <section class="flex flex-wrap pt-6 pb-6 lg:pt-16 lg:pb-16">
                 <div class="w-full px-4 lg:w-1/2 lg:pr-12">
-                    <h1 class="mb-6 text-3xl font-extrabold leading-none">Optimalizace procesu vypracování smluv</h1>
-                    <div class="inline-block mb-5 text-green-500">M&M reality a.s.</div>
-                    <div class="mb-6 text-base wysiwyg-content">Prodej či nákup nemovitosti je záležitost, kterou je třeba bezchybně smluvně ošetřit. V rámci služby realitní makléři poskytují svým klientům právní servis (ve spolupráci s právním oddělením).</div>
+                    <h1 class="mb-4 text-xl font-extrabold leading-none lg:mb-6 lg:text-3xl">Optimalizace procesu vypracování smluv</h1>
+                    <div class="inline-block mb-3 text-green-500 lg:mb-5">M&M reality a.s.</div>
+                    <div class="mb-4 text-xs lg:mb-6 lg:text-base wysiwyg-content">Prodej či nákup nemovitosti je záležitost, kterou je třeba bezchybně smluvně ošetřit. V rámci služby realitní makléři poskytují svým klientům právní servis (ve spolupráci s právním oddělením).</div>
                 </div>
                 <div class="w-full px-4 lg:w-1/2">
                     <img src="https://barth-net.cz/data/vehicle/car/photos/image_size_1024_768_inset_75/skoda-fabia-combi-style-1-0tsi-81kw5e455107e561c.JPG"
@@ -19,8 +19,8 @@
             <div class="relative py-8 ml-auto mr-auto max-w-screen-xl">
                 <section class="flex flex-wrap">
                     <div class="self-center w-full px-4 lg:w-14/24 lg:pr-12">
-                        <h2 class="mb-8 text-2xl font-bold">Výzva</h2>
-                        <div class="text-base wysiwyg-content">
+                        <h2 class="mb-3 text-base font-bold lg:mb-8 lg:text-2xl">Výzva</h2>
+                        <div class="text-xs lg:text-base wysiwyg-content">
                             <p>
                                 Majitelé nemovitostí chtějí od realitních makléřů špičkovou službu - nemovitost na prohlídce
                                 dobře odprezentovat a předat zájemcům všechny potřebné informace, aby se mohli rozhodnout.
@@ -36,9 +36,9 @@
                     <div class="self-start w-full px-4 lg:w-10/24 group">
                         <!--TODO Create component-->
                         <div class="bg-white rounded group-hover:shadow-xl">
-                            <div class="pt-16 mx-16">
-                                <img src="https://www.mmreality.cz/data/imgs/logo.png" alt="logo partnera" class="mb-8">
-                                <div class="wysiwyg-content">
+                            <div class="pt-4 mx-4  lg:pt-16 lg:mx-16">
+                                <img src="https://www.mmreality.cz/data/imgs/logo.png" alt="logo partnera" class="w-24 mb-4 lg:mb-8">
+                                <div class="text-xs wysiwyg-content lg:text-base">
                                     <p>
                                     M&M reality je největší a neúspěšnější česká realitní kancelář. Ročně zodbchoduje
                                     přes 30 000 nemovitostí a věnuje také developerské a investiční činnosti.
@@ -46,7 +46,7 @@
                                     od Hospodářské komory ČR.</p>
                                 </div>
                             </div>
-                            <a href="#client-survey" class="inline-flex items-center py-2 pl-16 mb-16 text-xs font-bold bg-gray-300 rounded-r-full group-hover:text-white group-hover:bg-gradient-r-blue-green">
+                            <a href="#client-survey" class="inline-flex items-center py-3 pl-4 mb-4 text-xs font-bold bg-gray-100 rounded-r-full lg:py-2 lg:pl-16 lg:mb-16 group-hover:text-white group-hover:bg-gradient-r-blue-green">
                                 Co k zakázce řekl klient?
                             </a>
                         </div>
@@ -56,10 +56,10 @@
             </div>
         </div>
         <div class="relative ml-auto mr-auto max-w-screen-xl">
-            <section class="flex flex-wrap mb-8">
+            <section class="flex flex-wrap mb-4 lg:mb-8">
                 <div class="self-center w-full px-4 lg:w-14/24 lg:pr-12">
-                    <h2 class="mb-8 text-2xl font-bold">Řešení</h2>
-                    <div class="text-base wysiwyg-content">
+                    <h2 class="mb-3 text-base font-bold lg:mb-8 lg:text-2xl">Řešení</h2>
+                    <div class="text-xs md:text-base wysiwyg-content">
                         <p class="">Vytvořili jsme nástroj, díky kterému můžu makléř vytvořit profesionální katalog skutečně rychle - do
                             10min. Předvyplníme mu popis a tech. parametry nemovitosti, doplníme list vlastnictví z katastru nemovitostí, přidáme představení makléře podle jeho skutečných referencí a doplníme cross-selling služby. Makléři mají možnost si katalog různě upravovat:
                         </p>
@@ -71,13 +71,13 @@
                     </div>
                 </div>
                 <div class="self-start w-full px-4 lg:w-10/24">
-                    <div class="mb-8 ml-16">
-                        <h3 class="mb-8 text-xl font-bold">Dodané služby</h3>
-                        <ul class="mb-16">
+                    <div class="mb-2 lg:mb-8 lg:ml-16">
+                        <h3 class="mb-4 text-base font-bold lg:mb-8 lg:text-xl">Dodané služby</h3>
+                        <ul class="mb-6 lg:mb-16">
                             <li class="mb-3">
                                 <div class="flex items-center mb-1">
-                                    <span class="flex-1 text-lg truncate">Projektové řízení</span>
-                                    <span class="flex-grow-0">20%</span>
+                                    <span class="flex-1 text-xs truncate lg:text-lg">Projektové řízení</span>
+                                    <span class="flex-grow-0 text-xs lg:text-sm">20%</span>
                                 </div>
                                 <div class="flex items-center">
                                     <div class="h-1 rounded bg-gradient-l-blue-green" style="width: 20%"></div>
@@ -86,8 +86,8 @@
                             </li>
                             <li class="mb-3">
                                 <div class="flex items-center mb-1">
-                                    <span class="flex-1 text-lg truncate">Vývoj</span>
-                                    <span class="flex-grow-0">80%</span>
+                                    <span class="flex-1 text-xs truncate lg:text-lg">Vývoj</span>
+                                    <span class="flex-grow-0 text-xs lg:text-sm">80%</span>
                                 </div>
                                 <div class="flex items-center">
                                     <div class="h-1 rounded bg-gradient-l-blue-green" style="width: 80%"></div>
@@ -96,8 +96,8 @@
                             </li>
                             <li class="mb-3">
                                 <div class="flex items-center mb-1">
-                                    <span class="flex-1 text-lg truncate">Bakendarna</span>
-                                    <span class="flex-grow-0">20%</span>
+                                    <span class="flex-1 text-xs truncate lg:text-lg">Bakendarna</span>
+                                    <span class="flex-grow-0 text-xs lg:text-sm">20%</span>
                                 </div>
                                 <div class="flex items-center">
                                     <div class="h-1 rounded bg-gradient-l-blue-green" style="width: 20%"></div>
@@ -105,10 +105,10 @@
                                 </div>
                             </li>
                         </ul>
-                        <h3 class="mb-8 text-xl font-bold">Technologie</h3>
+                        <h3 class="mb-4 text-base font-bold lg:mb-8 lg:text-xl">Technologie</h3>
 
                         <div class="lg:w-20/24">
-                            <div class="grid grid-cols-4 gap-4">
+                            <div class="grid grid-cols-3 md:grid-cols-4 gap-4">
                                 <div class="flex items-center justify-center p-2 border border-gray-100 rounded">
                                     <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="symfony">
                                 </div>
@@ -131,14 +131,14 @@
                 </div>
             </section>
         </div>
-        <div class="relative mb-16">
+        <div class="relative mb-6 lg:mb-16">
             <div class="absolute w-full h-full overflow-hidden">
                 <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full -mt-20" fit="cover"/>
             </div>
-            <div class="pt-8 ml-auto mr-auto max-w-screen-lg">
-                <VueSlickCarousel class="overflow-hidden rounded shadow-xl" v-bind="carouselSettings">
+            <div class="px-4 pt-8 ml-auto mr-auto max-w-screen-lg">
+                <VueSlickCarousel class="rounded shadow-xl" v-bind="carouselSettings">
                     <div class="relative">
-                        <div class="flex flex-col justify-around">
+                        <div class="flex flex-col justify-around overflow-hidden rounded">
                             <div class="relative aspect-ratio-16/9">
                                 <img src="https://i.picsum.photos/id/831/300/300.jpg" class="absolute inset-0 w-full h-full" />
                             </div>
@@ -148,7 +148,7 @@
                         </div>
                     </div>
                     <div class="relative">
-                        <div class="flex flex-col justify-around">
+                        <div class="flex flex-col justify-around overflow-hidden rounded">
                             <div class="relative aspect-ratio-16/9">
                                 <img src="https://i.picsum.photos/id/831/300/300.jpg" class="absolute inset-0 w-full h-full" />
                             </div>
@@ -177,30 +177,30 @@
             </div>
         </div>
         <div class="ml-auto mr-auto max-w-screen-xl">
-            <section class="flex flex-wrap pb-16" >
-                <div class="relative w-full px-4 lg:w-1/2 lg:pr-16">
-                    <h2 class="mb-8 text-2xl font-bold">Výsledek</h2>
-                    <div class="mb-6 text-base wysiwyg-content">
+            <section class="flex flex-wrap pb-6 lg:pb-16" >
+                <div class="relative w-full px-4 mb-6 lg:mb-0 lg:w-1/2 lg:pr-16">
+                    <h2 class="mb-3 text-base font-bold lg:mb-8 lg:text-2xl">Výsledek</h2>
+                    <div class="mb-6 text-xs lg:text-base wysiwyg-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus et eveniet necessitatibus nisi pariatur, porro quae quia quis recusandae repellendus sequi tenetur ut vero? Animi, dolores, hic. Iusto, omnis, quibusdam?</p>
                     </div>
-                    <div class="absolute inset-y-0 right-0 w-1 h-full rounded bg-gradient-t-blue-green"></div>
+                    <div class="absolute left-0 lg:left-auto right-0 h-1 mx-4 rounded lg-mx-0 lg:inset-y-0 lg:w-1 lg:h-full bg-gradient-r-blue-green lg:bg-gradient-t-blue-green"></div>
                 </div>
                 <div class="w-full px-4 lg:w-1/2 lg:pl-16" id="client-survey">
                     <div class="lg:w-18/24">
-                        <h2 class="mb-8 text-2xl font-bold text-green-500">Vyjádření klienta</h2>
+                        <h2 class="mb-3 text-base font-bold text-green-500 lg:mb-8 lg:text-2xl">Vyjádření klienta</h2>
                         <div class="relative">
-                            <icon symbol="i_quotation" class="absolute w-16 h-16 mt-8 ml-6 text-gray-100 fill-current transform -translate-x-full -translate-y-full"></icon>
-                            <div class="relative text-lg wysiwyg-content">
+                            <icon symbol="i_quotation" class="absolute w-16 h-16 -mt-3 -ml-3 text-gray-100 fill-current lg:mt-8 lg:ml-6 transform lg:-translate-x-full lg:-translate-y-full"></icon>
+                            <div class="relative text-sm lg:text-lg wysiwyg-content">
                                 <p>
                                     “We enjoy working with Spiria because they have proven to be experienced, professional and reliable. Since 2004, Spiria has helped us with a variety of complex projects and they continue to be a valued partner for expert development services.”
                                 </p>
                             </div>
                         </div>
                         <div class="flex items-center">
-                            <img class="flex-shrink-0 w-24 h-24 mr-4 rounded-full" src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="avatar">
+                            <img class="flex-shrink-0 w-16 h-16 mr-4 rounded-full lg:w-24 lg:h-24" src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/12/1418948033symfony-logo.png" alt="avatar">
                             <div>
-                                <strong class="text-lg">Zdeněk samek</strong>
-                                <div class="text-base">regionální ředitel pro Prahu a , M&M reality</div>
+                                <strong class="text-sm lg:text-lg">Zdeněk samek</strong>
+                                <div class="text-xs lg:text-base">regionální ředitel pro Prahu a , M&M reality</div>
                             </div>
                         </div>
                     </div>
@@ -208,17 +208,19 @@
             </section>
         </div>
         <div class="bg-gradient-r-blue-green">
-            <div class="ml-auto mr-auto max-w-screen-xl">
-                <div class="flex">
-                    <div class="w-1/2">
-                        <a href="#nogo" class="inline-flex items-center py-6 text-xl text-white">
-                            <icon symbol="i_chevron" class="w-3 h-3 mr-2 fill-current transform rotate-180"></icon>
-                            Zpět na seznam referencí
-                        </a>
+            <div class="ml-auto mr-auto max-w-screen-xl px-4">
+                <div class="flex flex-wrap-reverse">
+                    <div class="w-full lg:w-1/2">
+                        <div class="text-center lg:text-left">
+                            <a href="#nogo" class="inline-flex items-center py-6 text-base text-white lg:text-xl">
+                                <icon symbol="i_chevron" class="w-3 h-3 mr-2 fill-current transform rotate-180"></icon>
+                                Zpět na seznam referencí
+                            </a>
+                        </div>
                     </div>
-                    <div class="w-1/2">
-                        <div class="text-right">
-                            <a href="#nogo" class="inline-flex items-center py-6 text-xl text-white">
+                    <div class="w-full lg:w-1/2">
+                        <div class="text-center lg:text-right">
+                            <a href="#nogo" class="inline-flex items-center py-6 text-base text-white lg:text-xl">
                                 Další reference
                                 <icon symbol="i_chevron" class="w-3 h-3 ml-2 fill-current"></icon>
                             </a>
