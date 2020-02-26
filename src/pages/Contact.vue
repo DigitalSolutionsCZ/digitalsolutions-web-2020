f0758fbbe24b5843c524a6bab3951c03d7b44903
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-26 10:48:36 +0100

    Add response for contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,20 +1,20 @@
 <template>
     <Layout>
-        <div class="ml-auto mr-auto max-w-screen-xl w-14/24">
+        <div class="ml-auto mr-auto max-w-screen-xl md:w-20/24 xl:w-14/24">
             <section class="flex flex-wrap pt-6 pb-6 xl:pt-16 xl:pb-16">
                 <div class="px-4 text-center">
                     <h1 class="mb-3 text-xl font-extrabold leading-none md:mb-5 xl:mb-6 md:text-2xl xl:text-3xl">Buďme v kontaktu</h1>
-                    <p class="mb-4 text-base text-gray-700 xl:mb-8">
+                    <p class="mb-4 text-base text-gray-700 md:mb-6 xl:mb-8">
                         Ať už máte vizi a hledáte pro ni partnera, Máte ve firmě nějakou potřebu nebo byznys problém a chcete navrhnout řešení, máte hotové zadání.
                     </p>
-                    <div class="flex">
-                        <div class="w-8/24">
+                    <div class="md:flex">
+                        <div class="md:w-8/24">
                             <a href="#nogo">Napište nám</a>
                         </div>
-                        <div class="w-8/24">
-                            <div class="text-center">nebo vyplňte</div>
+                        <div class="md:w-8/24">
+                            <div class="text-base text-center">nebo vyplňte</div>
                         </div>
-                        <div class="w-8/24">
+                        <div class="md:w-8/24">
                             <a href="#nogo">Nezávaznou poptávku</a>
                         </div>
 
@@ -22,58 +22,60 @@
                 </div>
             </section>
         </div>
-        <div class="relative mb-16">
+        <div class="relative mb-4 md:mb-8 xl:mb-16">
             <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full" fit="cover"/>
-            <div class="relative py-8 ml-auto mr-auto max-w-screen-xl">
-                <section class="flex flex-wrap bg-white rounded">
-                    <div class="w-15/24">
-                        <a href="https://www.google.com/maps/place/17.+listopadu+203,+530+02+Pardubice+I-Zelen%C3%A9+P%C5%99edm%C4%9Bst%C3%AD,+%C4%8Cesko/@50.0353454,15.769444,19.25z/data=!4m5!3m4!1s0x470dcc95af98e1df:0x85830a9da8603edc!8m2!3d50.0353388!4d15.7697321"
-                           target="_blank"
-                           class="block h-full"
-                        >
-                            <g-image src="~/images/contact-map.jpg" alt="mapa" class="object-cover w-full h-full" fit="cover"/>
-                        </a>
-                    </div>
-                    <div class="w-9/24">
-                        <div class="py-12 pl-8 pr-4">
-                            <div class="mb-4 text-lg font-bold xl:text-xl xl:mb-8">Digital Solutions s.r.o.</div>
-                            <div class="text-gray-900">
-                                <div class="flex items-center mb-2 text-base xl:text-lg">
-                                    <icon symbol="i_map_point" class="w-4 h-4 mr-3 text-green-500 fill-current flex-grow-0"></icon>
-                                    <span>17. listopadu 203, 530 02 Pardubice</span>
+            <div class="px-4 py-4 xl:py-8 md:px-16">
+                <div class="relative ml-auto mr-auto max-w-screen-xl">
+                    <section class="flex flex-wrap overflow-hidden bg-white rounded">
+                        <div class="w-full md:w-15/24">
+                            <a href="https://www.google.com/maps/place/17.+listopadu+203,+530+02+Pardubice+I-Zelen%C3%A9+P%C5%99edm%C4%9Bst%C3%AD,+%C4%8Cesko/@50.0353454,15.769444,19.25z/data=!4m5!3m4!1s0x470dcc95af98e1df:0x85830a9da8603edc!8m2!3d50.0353388!4d15.7697321"
+                               target="_blank"
+                               class="block h-full"
+                            >
+                                <g-image src="~/images/contact-map.jpg" alt="mapa" class="object-cover w-full h-full" fit="cover"/>
+                            </a>
+                        </div>
+                        <div class="w-full: md:w-9/24">
+                            <div class="py-12 pl-8 pr-4">
+                                <div class="mb-4 text-lg font-bold xl:text-xl xl:mb-8">Digital Solutions s.r.o.</div>
+                                <div class="text-gray-900 text-sm md:text-base xl:text-lg">
+                                    <div class="flex mb-2">
+                                        <icon symbol="i_map_point" class="flex-grow-0 w-4 h-4 mr-3 mt-1 text-green-500 fill-current"></icon>
+                                        <span>17. listopadu 203, 530 02 Pardubice</span>
+                                    </div>
+                                    <div class="flex mb-2 text-base xl:text-lg">
+                                        <icon symbol="i_phone" class="flex-grow-0 w-4 h-4 mr-3 mt-1 text-green-500 fill-current"></icon>
+                                        <strong>+420 775 300 500</strong>
+                                    </div>
+                                    <div class="flex mb-4 text-base xl:text-lg">
+                                        <icon symbol="i_envelope" class="flex-grow-0 w-4 h-4 mr-3  mt-1 text-green-500 fill-current"></icon>
+                                        <span>info@digital.cz</span>
+                                    </div>
+                                    <p>IČ: 25998706, DIČ: CZ25998706</p>
+                                    <p>Datová schránka ID: tmeyy55</p>
                                 </div>
-                                <div class="flex items-center mb-2 text-base xl:text-lg">
-                                    <icon symbol="i_phone" class="w-4 h-4 mr-3 text-green-500 fill-current flex-grow-0"></icon>
-                                    <span>+420 775 300 500</span>
-                                </div>
-                                <div class="flex items-center mb-4 text-base xl:text-lg">
-                                    <icon symbol="i_envelope" class="w-4 h-4 mr-3 text-green-500 fill-current flex-grow-0"></icon>
-                                    <span>info@digital.cz</span>
-                                </div>
-                                <p>IČ: 25998706, DIČ: CZ25998706</p>
-                                <p>Datová schránka ID: tmeyy55</p>
                             </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
         </div>
         <div class="px-4">
             <div class="ml-auto mr-auto max-w-screen-xl">
-                <section class="mb-4 lg:mb-8 mx-4">
+                <section class="mx-4 mb-4 lg:mb-8">
                     <div class="px-4 text-center">
-                        <h2 class="mb-3 text-base font-bold md:mb-5 xl:mb-8 md:text-xl xl:text-2xl">Jak se k nám dostanete</h2>
-                        <div class="text-xs md:text-sm xl:text-base w-14/24 text-gray-900 m-auto wysiwyg-content">
+                        <h2 class="mb-3 text-base font-bold xl:mb-8 md:text-xl xl:text-2xl">Jak se k nám dostanete</h2>
+                        <div class="m-auto text-sm text-gray-900 xl:text-base md:w-20/24 xl:w-14/24 wysiwyg-content">
                             <p class="">
                                 Rádi se s našimi klienty potkáváme osobně. Takže ať už si zvolíte jakoukoliv cestu, <strong>rádi Vás přivítáme v našich kancelářích</strong> dobrou kávou.
                             </p>
                         </div>
                     </div>
                 </section>
-                <div class="flex">
-                    <div class="w-6/24 pr-16">
-                        <div class="font-bold text-lg text-blue-500 mb-2">Autem</div>
-                        <div class="wysiwyg-content text-gray-700 text-base break-words">
+                <div class="flex flex-wrap mb-2">
+                    <div class="w-full md:w-7/24 xl:w-6/24 md:pr-10 xl:pr-16">
+                        <div class="mb-2 font-bold text-blue-500 md:text-base xl:text-lg">Autem</div>
+                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
                             Naše parkoviště je na souřadnicích: 50°02’07.3“N 15°46’09.4“E
                             <a href="https://goo.gl/maps/AARg7XtiGshfZeVV8">https://goo.gl/maps/AARg7XtiGshfZeVV8</a>
                             <p>
@@ -82,9 +84,9 @@
                             </p>
                         </div>
                     </div>
-                    <div class="w-12/24 pr-16">
-                        <div class="font-bold text-lg text-blue-500 mb-2">Vlakem</div>
-                        <div class="wysiwyg-content text-gray-700 text-base break-words">
+                    <div class="w-full md:w-10/24 xl:w-12/24 md:pr-10 xl:pr-16">
+                        <div class="mb-2 text-base font-bold text-blue-500 xl:text-lg">Vlakem</div>
+                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
                             <p>
                                 Pardubice jsou z historických důvodů velmi dobře dostupné vlakem, na frekventované trase Praha-Ostrava nebo Praha-Brno.
                                 Naplánujte si trasu vlakem na Idos.cz
@@ -99,9 +101,9 @@
                             </p>
                         </div>
                     </div>
-                    <div class="w-6/24">
-                        <div class="font-bold text-lg text-blue-500 mb-2">Pro klienty z Pardubic</div>
-                        <div class="wysiwyg-content text-gray-700 text-base break-words">
+                    <div class="w-full md:w-7/24 xl:w-6/24">
+                        <div class="mb-2 text-base font-bold text-blue-500 xl:text-lg">Pro klienty z Pardubic</div>
+                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
                             <p>
                                 Najdete nás na ulici 17. listopadu, kde sídlíme v nově zrekonstruované budově Banka CREDITAS (100 metrů od kulatého rohu) na pravé straně silnice.
                                 Naše souřadnice: 50°02’07.4“N 15°46’11.1“E <a href="#nogo">https://goo.gl/maps/poZjXohazF8kV3nF7</a>
@@ -112,23 +114,23 @@
                         </div>
                     </div>
                 </div>
-                <h3 class="font-bold text-lg md:text-xl mb-8 text-center">Napište nám</h3>
-                <div class="w-10/24 mx-auto">
-                    <div class="flex flex-wrap mb-8">
+                <h3 class="mb-8 text-lg font-bold text-center md:text-xl">Napište nám</h3>
+                <div class="mx-auto md:w-10/24">
+                    <div class="flex flex-wrap">
                         <div class="w-full">
-                            <input type="text" class="w-full border border-gray-200 rounded h-10 px-3 mb-8 placeholder-gray-200" placeholder="Jméno a příjmení">
+                            <input type="text" class="w-full h-10 px-3 mb-4 md:mb-8 placeholder-gray-200 border border-gray-200 rounded" placeholder="Jméno a příjmení">
                         </div>
-                        <div class="w-12/24 pr-4">
-                            <input type="text" class="w-full border border-gray-200 rounded h-10 px-3 mb-8 placeholder-gray-200" placeholder="Email">
+                        <div class="pr-4 w-12/24">
+                            <input type="text" class="w-full h-10 px-3 mb-4 md:mb-8 placeholder-gray-200 border border-gray-200 rounded" placeholder="Email">
                         </div>
                         <div class="w-12/24">
-                            <input type="text" class="w-full border border-gray-200 rounded h-10 px-3 mb-8 placeholder-gray-200" placeholder="Telefon">
+                            <input type="text" class="w-full h-10 px-3 mb-4 md:mb-8 placeholder-gray-200 border border-gray-200 rounded" placeholder="Telefon">
                         </div>
                         <div class="w-full">
-                            <textarea class="w-full border border-gray-200 rounded px-3 pt-3 placeholder-gray-200" rows="4" placeholder="Popište nám prosím váš projekt nebo potřeby…"></textarea>
+                            <textarea class="w-full px-3 pt-3 placeholder-gray-200 border border-gray-200 rounded mb-4 md:mb-8" rows="4" placeholder="Popište nám prosím váš projekt nebo potřeby…"></textarea>
                         </div>
                     </div>
-                    <button class="mb-16">Odeslat</button>
+                    <button class="mb-4 md:mb-16">Odeslat</button>
                 </div>
             </div>
         </div>
@@ -136,13 +138,13 @@
             <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full" fit="cover"/>
             <div class="px-4">
                 <div class="relative pt-24 pb-20 ml-auto mr-auto max-w-screen-xl">
-                    <h3 class="font-bold text-lg md:text-xl mb-8 text-center">Sledujte nás na sociálních sítích</h3>
+                    <h3 class="mb-3 text-lg font-bold text-center md:text-xl md:mb-6 xl:mb-8">Sledujte nás na sociálních sítích</h3>
                     <div>
-                        <div class="flex flex-wrap justify-center">
-                            <icon symbol="i_facebook" class="w-12 h-12 mr-8 mb-4"></icon>
-                            <icon symbol="i_linkedin" class="w-12 h-12 mr-8 mb-4"></icon>
-                            <icon symbol="i_twitter" class="w-12 h-12 mr-8 mb-4"></icon>
-                            <icon symbol="i_instagram" class="w-12 h-12"></icon>
+                        <div class="flex flex-wrap items-center justify-center">
+                            <icon symbol="i_facebook" class="w-16 h-16 mb-4 mr-1"></icon>
+                            <icon symbol="i_twitter" class="w-20 h-20 mb-4"></icon>
+                            <icon symbol="i_linkedin" class="w-16 h-16 mb-4 mr-2"></icon>
+                            <icon symbol="i_instagram" class="w-16 h-16 mb-4"></icon>
                         </div>
                     </div>
                 </div>
