c7d42dc177092406c64f2fa9330a935a7a7855ec
Jan Dusek <jan.dusek@digitalcz.cz>
2020-03-02 13:24:07 +0100

    Integrate chameleon header

---
+++ src/components/ChameleonHeader.js
@@ -0,0 +1,168 @@
+const ChaHeader = function (element) {
+  this.element = element;
+  this.sections = document.querySelectorAll('[data-cha-section]'); //js-cha-section
+  this.header = this.element.querySelector('[data-cha-header]'); // js-cha-header
+  // handle mobile behaviour
+  this.headerTrigger = this.element.getElementsByClassName('js-cha-header__trigger');
+  this.modal = document.getElementsByClassName('js-cha-modal');
+  this.focusMenu = false;
+  this.firstFocusable = null;
+  this.lastFocusable = null;
+  initChaHeader(this);
+};
+
+function initChaHeader(element) {
+  // set initial status
+  console.log(element)
+  for (let j = 0; j < element.sections.length; j++) {
+    initSection(element, j);
+  }
+
+  // handle mobile behaviour
+  if (element.headerTrigger.length > 0) {
+    // initMobileVersion(element);
+  }
+
+  // make sure header element is visible when in focus
+  element.header.addEventListener('focusin', (event) => {
+    checkHeaderVisible(element);
+  });
+}
+
+function initSection(element, index) {
+  // clone header element inside each section
+  const cloneItem = (index === 0) ? element.element : element.element.cloneNode(true);
+
+  cloneItem.removeAttribute('[data-cha-header-clip]');
+  const customClasses = element.sections[index].getAttribute('data-header-class');
+  // hide clones to SR
+  cloneItem.setAttribute('aria-hidden', 'true');
+  if (customClasses) {
+    cloneItem.querySelector('[data-cha-header]').classList.add(customClasses)
+  }
+  // keyborad users - make sure cloned items are not tabbable
+  if (index !== 0) {
+    // reset tab index
+    resetTabIndex(cloneItem);
+    element.sections[index].insertBefore(cloneItem, element.sections[index].firstChild);
+  }
+}
+
+function resetTabIndex(clone) {
+  const focusable = clone.querySelectorAll('[href], button, input');
+  for (let i = 0; i < focusable.length; i++) {
+    focusable[i].setAttribute('tabindex', '-1');
+  }
+}
+
+function initMobileVersion(element) {
+  // detect click on nav trigger
+  const triggers = document.getElementsByClassName('js-cha-header__trigger');
+  for (let i = 0; i < triggers.length; i++) {
+    triggers[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      const ariaExpanded = !Util.hasClass(element.modal[0], 'is-visible');
+      // show nav and update button aria value
+      Util.toggleClass(element.modal[0], 'is-visible', ariaExpanded);
+      element.headerTrigger[0].setAttribute('aria-expanded', ariaExpanded);
+      if (ariaExpanded) { // opening menu -> move focus to first element inside nav
+        getFocusableElements(element);
+        element.firstFocusable.focus();
+      } else if (element.focusMenu) {
+        if (window.scrollY < element.focusMenu.offsetTop) element.focusMenu.focus();
+        element.focusMenu = false;
+      }
+    });
+  }
+
+
+  // listen for key events
+  window.addEventListener('keydown', (event) => {
+    // listen for esc key
+    if ((event.keyCode && event.keyCode === 27) || (event.key && event.key.toLowerCase() === 'escape')) {
+      // close navigation on mobile if open
+      if (element.headerTrigger[0].getAttribute('aria-expanded') === 'true' && isVisible(element.headerTrigger[0])) {
+        closeModal(element);
+      }
+    }
+    // listen for tab key
+    if ((event.keyCode && event.keyCode === 9) || (event.key && event.key.toLowerCase() === 'tab')) {
+      trapFocus(element, event);
+    }
+  });
+}
+
+function closeModal(element) {
+  element.focusMenu = element.headerTrigger[0]; // move focus to menu trigger when menu is close
+  element.headerTrigger[0].click();
+}
+
+function trapFocus(element, event) {
+  if (element.firstFocusable === document.activeElement && event.shiftKey) {
+    // on Shift+Tab -> focus last focusable element when focus moves out of modal
+    event.preventDefault();
+    element.lastFocusable.focus();
+  }
+  if (element.lastFocusable === document.activeElement && !event.shiftKey) {
+    // on Tab -> focus first focusable element when focus moves out of modal
+    event.preventDefault();
+    element.firstFocusable.focus();
+  }
+}
+
+function getFocusableElements(element) {
+  // get all focusable elements inside the modal
+  const allFocusable = element.modal[0].querySelectorAll('[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex]:not([tabindex="-1"]), [contenteditable], audio[controls], video[controls], summary');
+  getFirstVisible(element, allFocusable);
+  getLastVisible(element, allFocusable);
+}
+
+function getFirstVisible(element, elements) {
+  // get first visible focusable element inside the modal
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].offsetWidth || elements[i].offsetHeight || elements[i].getClientRects().length) {
+      element.firstFocusable = elements[i];
+      return true;
+    }
+  }
+}
+
+function getLastVisible(element, elements) {
+  // get last visible focusable element inside the modal
+  for (let i = elements.length - 1; i >= 0; i--) {
+    if (elements[i].offsetWidth || elements[i].offsetHeight || elements[i].getClientRects().length) {
+      element.lastFocusable = elements[i];
+      return true;
+    }
+  }
+}
+
+function checkHeaderVisible(element) {
+  if (window.scrollY > element.sections[0].offsetHeight - element.header.offsetHeight) window.scrollTo(0, 0);
+}
+
+function isVisible(element) {
+  return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+}
+
+function cssSupports(property, value) {
+  if ('CSS' in window) {
+    return CSS.supports(property, value);
+  }
+  const jsProperty = property.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+  return jsProperty in document.body.style;
+}
+
+// init the ChaHeader Object
+export default function init () {
+  const chaHeader = document.querySelectorAll('[data-cha-header-clip]') || [];
+  const clipPathSupported = cssSupports('clip-path', 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)') || cssSupports('-webkit-clip-path', 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)');
+  if (chaHeader.length > 0 && clipPathSupported) {
+    for (let i = 0; i < chaHeader.length; i++) {
+      console.log(chaHeader)
+      new ChaHeader(chaHeader[i]);
+    }
+  }
+}
+
+
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,26 +1,101 @@
 <template>
   <Layout>
-    <div class="h-screen bg-gradient-tr-blue-green flex">
-      <section class="w-full self-center">
-        <div class="max-w-6xl mx-auto">
-          <div class="text-center">
-            <h1 class="xl:text-3xl text-white xl:mb-8 leading-none">Vyvíjíme <span class="font-extrabold">informační systémy</span>, webové a&nbsp;mobilní aplikace na míru</h1>
-            <div class="opacity-75 text-white mb-10">I na ty opravdu velké si sáhnete do<br> 3 měsíců</div>
-            <g-link class="inline-flex justify-center px-4 py-2 text-sm border border-gray-100 rounded-full min-w-40 md:text-base text-white hover:shadow-lg transition-all duration-200 ease-in-out" to="/contact/">
-              Jak vyvíjíme
-            </g-link>
+    <template #header>
+      <section class="relative" data-cha-section data-theme="dark">
+        <div class="cha-header-clip" data-cha-header-clip>
+          <div class="fixed inset-x-0 top-0 z-30 px-4 cha-header" style="background: var(--header-background)" >
+            <header class="mx-auto max-w-screen-3xl" data-cha-header>
+              <div class="flex items-center h-12 md:h-20 xl:h-24">
+                <div class="sm:w-7/24 w-9/24 xs:w-10/24 md:w-5/24 xl:w-4/24">
+                  <g-link to="/" class="relative flex items-center h-12 md:h-20 xl:h-24">
+                    <img src="../images/logo-ds.png"  style="visibility: var(--logo-color-visibility)" class="absolute" />
+                    <img src="../images/logo-ds-white.png" style="visibility: var(--logo-white-visibility)"  class="absolute" />
+                  </g-link>
+                </div>
+                <div class="flex-grow hidden md:block">
+                  <nav class="flex justify-center text-base font-bold cha-nav">
+                    <g-link class="cha-nav-link group" to="/about/">
+                      <div class="relative">
+                        O nás
+                        <div class="cha-nav-link-underline"></div>
+                      </div>
+                    </g-link>
+                    <g-link class="cha-nav-link" to="/services/">
+                      <div class="relative">
+                        Služby
+                        <div class="cha-nav-link-underline"></div>
+                      </div>
+                    </g-link>
+                    <g-link class="cha-nav-link" to="/reference/">
+                      <div class="relative">
+                        Reference
+                        <div class="cha-nav-link-underline"></div>
+                      </div>
+                    </g-link>
+                    <g-link class="cha-nav-link" to="/demand/">
+                      <div class="relative">
+                        Poptávka
+                        <div class="cha-nav-link-underline"></div>
+                      </div>
+                    </g-link>
+                    <g-link class="cha-nav-link" to="/contact/">
+                      <div class="relative">
+                        Kontakt
+                        <div class="cha-nav-link-underline"></div>
+                      </div>
+                    </g-link>
+                  </nav>
+                </div>
+                <div class="w-15/24 md:w-3/24 xl:w-3/24">
+                  <div class="flex items-center justify-end">
+                    <g-link class="cha-nav-button" to="/contact/">
+                      Kontakt
+                    </g-link>
+                    <div class="ml-3 md:hidden">☰</div>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </div>
+        </div>
+        <div class="flex h-screen bg-gradient-tr-blue-green">
+          <section class="self-center w-full">
+            <div class="max-w-6xl mx-auto">
+              <div class="text-center">
+                <h1 class="leading-none text-white xl:text-3xl xl:mb-8">Vyvíjíme <span class="font-extrabold">informační systémy</span>, webové a&nbsp;mobilní aplikace na míru</h1>
+                <div class="mb-10 text-white opacity-75">I na ty opravdu velké si sáhnete do<br> 3 měsíců</div>
+                <g-link class="inline-flex justify-center px-4 py-2 text-sm text-white border border-gray-100 rounded-full min-w-40 md:text-base hover:shadow-lg transition-all duration-200 ease-in-out" to="/contact/">
+                  Jak vyvíjíme
+                </g-link>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </template>
+    <template #content>
+      <section class="relative" data-cha-section>
+        <!-- your section content 👇-->
+        <div class="container">
+          <div class="text-component line-height-lg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus et fugiat reiciendis maiores. Illo, ex magnam, soluta, accusamus rerum incidunt provident deserunt enim neque tempora dicta asperiores expedita veritatis rem perferendis modi! Eos, aperiam delectus itaque repudiandae sit necessitatibus mollitia nostrum maiores? Perspiciatis commodi sunt voluptates similique! Dolorem nobis repellat laborum praesentium maxime fugit id non eius doloremque? Corporis suscipit cupiditate eaque vitae voluptates tempora repellat? Laboriosam facere architecto illo repellat. Reprehenderit odio iure nihil obcaecati esse, laboriosam rerum quod quidem natus perferendis, officiis tempore eos veritatis aspernatur cumque corrupti a, minima non tempora! Nam adipisci at repellendus optio earum.</p>
           </div>
         </div>
       </section>
-    </div>
+    </template>
   </Layout>
 </template>
 
 <script>
+import init from '../components/ChameleonHeader.js';
+
 export default {
   metaInfo: {
     title: 'Hello, world!'
   },
+  mounted() {
+    init();
+  }
 }
 </script>
 
