d67d6f95b9507771f1bd417fd7330489a5cd5dad
Jan Dusek <jan.dusek@digitalcz.cz>
2020-03-04 09:46:45 +0100

    Data binding of contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,21 +1,19 @@
 <template>
     <Layout>
-        <div class="ml-auto mr-auto max-w-screen-xl md:w-18/24 xl:w-14/24">
+        <div class="max-w-screen-xl ml-auto mr-auto md:w-18/24 xl:w-14/24">
             <section class="flex flex-wrap pt-6 pb-6 xl:pt-16 xl:pb-16">
                 <div class="px-4 text-center">
-                    <h1 class="mb-3 text-xl font-extrabold leading-none md:mb-5 xl:mb-6 md:text-2xl xl:text-3xl">Buďme v kontaktu</h1>
-                    <p class="mb-4 text-base text-gray-700 md:mb-6 xl:mb-8">
-                        Ať už máte vizi a hledáte pro ni partnera, Máte ve firmě nějakou potřebu nebo byznys problém a chcete navrhnout řešení, máte hotové zadání.
-                    </p>
-                    <div class="items-center md:flex">
+                    <h1 class="mb-3 text-xl font-extrabold leading-none md:mb-5 xl:mb-6 md:text-2xl xl:text-3xl">{{ page.heading }}</h1>
+                    <div class="mb-4 text-base text-gray-700 md:mb-6 xl:mb-8" v-html="page.excerpt"></div>
+                    <div class="items-center md:flex" v-if="page.showContactButtons">
                         <div class="md:w-9/24">
-                            <a href="#nogo" class="flex items-center justify-center px-4 py-3 mx-2 mb-3 text-base font-semibold bg-gray-100 rounded-full min-w-40 hover:shadow-lg transition-all duration-200 ease-in-out active">Napište nám</a>
+                            <a href="#nogo" class="flex items-center justify-center px-4 py-3 mx-2 mb-3 text-base font-semibold transition-all duration-200 ease-in-out bg-gray-100 rounded-full min-w-40 hover:shadow-lg active">Napište nám</a>
                         </div>
                         <div class="md:w-6/24">
                             <div class="mb-3 text-base text-center">nebo vyplňte</div>
                         </div>
                         <div class="md:w-9/24">
-                            <a href="#nogo" class="flex items-center justify-center px-4 py-3 mx-2 mb-3 text-base font-semibold text-white rounded-full min-w-40 hover:shadow-lg transition-all duration-200 ease-in-out bg-gradient-r-blue-green active">Nezávaznou poptávku</a>
+                            <a href="#nogo" class="flex items-center justify-center px-4 py-3 mx-2 mb-3 text-base font-semibold text-white transition-all duration-200 ease-in-out rounded-full min-w-40 hover:shadow-lg bg-gradient-r-blue-green active">Nezávaznou poptávku</a>
                         </div>
                     </div>
                 </div>
@@ -24,34 +22,33 @@
         <div class="relative mb-4 md:mb-8 xl:mb-16">
             <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full" fit="cover"/>
             <div class="px-4 py-4 xl:py-8 md:px-16">
-                <div class="relative ml-auto mr-auto max-w-screen-xl">
+                <div class="relative max-w-screen-xl ml-auto mr-auto">
                     <section class="flex flex-wrap overflow-hidden bg-white rounded">
                         <div class="w-full md:w-15/24">
-                            <a href="https://www.google.com/maps/place/17.+listopadu+203,+530+02+Pardubice+I-Zelen%C3%A9+P%C5%99edm%C4%9Bst%C3%AD,+%C4%8Cesko/@50.0353454,15.769444,19.25z/data=!4m5!3m4!1s0x470dcc95af98e1df:0x85830a9da8603edc!8m2!3d50.0353388!4d15.7697321"
+                            <a :href="mapObject(page, ['map', 0, 'contactMapLink'])"
                                target="_blank"
                                class="block h-full"
                             >
-                                <g-image src="~/images/contact-map.jpg" alt="mapa" class="object-cover w-full h-full" fit="cover"/>
+                                <g-image :src="mapObject(page, ['map', 0, 'contactMapImage', 0, 'url'])" alt="mapa" class="object-cover w-full h-full" fit="cover"/>
                             </a>
                         </div>
                         <div class="w-full: md:w-9/24">
                             <div class="py-12 pl-8 pr-4">
-                                <div class="mb-4 text-lg font-bold xl:text-xl xl:mb-8">Digital Solutions s.r.o.</div>
+                                <div class="mb-4 text-lg font-bold xl:text-xl xl:mb-8">{{ mapObject(page, ['contactAdress', 0, 'header']) }}</div>
                                 <div class="text-sm text-gray-900 md:text-base xl:text-lg">
                                     <div class="flex mb-2">
-                                        <icon symbol="i_map_point" class="flex-grow-0 w-4 h-4 mt-1 mr-3 text-green-500 fill-current"></icon>
-                                        <span>17. listopadu 203, 530 02 Pardubice</span>
+                                        <icon symbol="i_map_point" class="flex-grow-0 w-6 h-6 mt-1 mr-3 text-green-500 fill-current"></icon>
+                                        <span>{{ mapObject(page, ['contactAdress', 0, 'address']) }}</span>
                                     </div>
                                     <div class="flex mb-2 text-base xl:text-lg">
-                                        <icon symbol="i_phone" class="flex-grow-0 w-4 h-4 mt-1 mr-3 text-green-500 fill-current"></icon>
-                                        <strong>+420 775 300 500</strong>
+                                        <icon symbol="i_phone" class="flex-grow-0 w-6 h-6 mt-1 mr-3 text-green-500 fill-current"></icon>
+                                        <strong>{{ mapObject(page, ['contactAdress', 0, 'phone']) }}</strong>
                                     </div>
                                     <div class="flex mb-4 text-base xl:text-lg">
-                                        <icon symbol="i_envelope" class="flex-grow-0 w-4 h-4 mt-1 mr-3 text-green-500 fill-current"></icon>
-                                        <span>info@digital.cz</span>
+                                        <icon symbol="i_envelope" class="flex-grow-0 w-6 h-6 mt-1 mr-3 text-green-500 fill-current"></icon>
+                                        <span>{{ mapObject(page, ['contactAdress', 0, 'email']) }}</span>
                                     </div>
-                                    <p>IČ: 25998706, DIČ: CZ25998706</p>
-                                    <p>Datová schránka ID: tmeyy55</p>
+                                    <p>{{ mapObject(page, ['contactAdress', 0, 'description']) }}</p>
                                 </div>
                             </div>
                         </div>
@@ -60,58 +57,20 @@
             </div>
         </div>
         <div class="px-4">
-            <div class="ml-auto mr-auto max-w-screen-xl">
+            <div class="max-w-screen-xl ml-auto mr-auto">
                 <section class="mx-4 mb-4 lg:mb-8">
                     <div class="px-4 text-center">
-                        <h2 class="mb-3 text-base font-bold xl:mb-8 md:text-xl xl:text-2xl">Jak se k nám dostanete</h2>
-                        <div class="m-auto text-sm text-gray-900 xl:text-base md:w-20/24 xl:w-14/24 wysiwyg-content">
-                            <p class="">
-                                Rádi se s našimi klienty potkáváme osobně. Takže ať už si zvolíte jakoukoliv cestu, <strong>rádi Vás přivítáme v našich kancelářích</strong> dobrou kávou.
-                            </p>
-                        </div>
+                        <h2 class="mb-3 text-base font-bold xl:mb-8 md:text-xl xl:text-2xl">{{ page.subheading }}</h2>
+                        <div class="m-auto text-sm text-gray-900 xl:text-base md:w-20/24 xl:w-14/24 wysiwyg-content" v-html="page.body"></div>
                     </div>
                 </section>
                 <div class="flex flex-wrap mb-2">
-                    <div class="w-full md:w-7/24 xl:w-6/24 md:pr-10 xl:pr-16">
-                        <div class="mb-2 font-bold text-blue-500 md:text-base xl:text-lg">Autem</div>
-                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
-                            Naše parkoviště je na souřadnicích: 50°02’07.3“N 15°46’09.4“E
-                            <a href="https://goo.gl/maps/AARg7XtiGshfZeVV8">https://goo.gl/maps/AARg7XtiGshfZeVV8</a>
-                            <p>
-                                Předem si prosím ověřte, zda je na parkovišti volné místo, případně Vám doporučíme další možnosti parkování.
-                                Bránu Vám otevře naše milá asistentka po zavolání na číslo 775 300 500 :-)
-                            </p>
+                    <template v-for="(route, index) in page.threeTextColumns">
+                        <div :class="getColumnSizeByOrder(index)" :key="route.id">
+                            <div class="mb-2 text-base font-bold text-blue-500 xl:text-lg">{{ route.header }}</div>
+                            <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base" v-html="route.description"></div>
                         </div>
-                    </div>
-                    <div class="w-full md:w-10/24 xl:w-12/24 md:pr-10 xl:pr-16">
-                        <div class="mb-2 text-base font-bold text-blue-500 xl:text-lg">Vlakem</div>
-                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
-                            <p>
-                                Pardubice jsou z historických důvodů velmi dobře dostupné vlakem, na frekventované trase Praha-Ostrava nebo Praha-Brno.
-                                Naplánujte si trasu vlakem na Idos.cz
-                            </p>
-                            <p>
-                                Pěšky (15min): Většina návštěv volí cestu z nádraží pěšky, je to kousek :-)
-                                Naše souřadnice: 50°02’07.4“N 15°46’11.1“E https://goo.gl/maps/poZjXohazF8kV3nF7
-                            </p>
-                            <p>
-                                Taxi (5min+):
-                                Doporučujeme TAXI GT (+420 777 456 456) nebo TAXI Vi3 (+420 778 158 158), obě služby zastavují na malém parkovišti K+R vlevo po východu z nádraží.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="w-full md:w-7/24 xl:w-6/24">
-                        <div class="mb-2 text-base font-bold text-blue-500 xl:text-lg">Pro klienty z Pardubic</div>
-                        <div class="text-sm text-gray-700 break-words wysiwyg-content xl:text-base">
-                            <p>
-                                Najdete nás na ulici 17. listopadu, kde sídlíme v nově zrekonstruované budově Banka CREDITAS (100 metrů od kulatého rohu) na pravé straně silnice.
-                                Naše souřadnice: 50°02’07.4“N 15°46’11.1“E <a href="#nogo">https://goo.gl/maps/poZjXohazF8kV3nF7</a>
-                            </p>
-                            <p>
-                                Zazvoňte nebo zavolejte na číslo 775 300 500 a naše milá asistentka Vám přijde otevřít :-)
-                            </p>
-                        </div>
-                    </div>
+                    </template>
                 </div>
                 <h3 class="mb-8 text-lg font-bold text-center md:text-xl">Napište nám</h3>
                 <div class="mx-auto md:w-10/24">
@@ -122,7 +81,7 @@
         <div class="relative">
             <g-image src="~/images/bg_ds_code.jpg" class="absolute object-cover w-full h-full" fit="cover"/>
             <div class="px-4">
-                <div class="relative pt-10 ml-auto mr-auto xl:pt-24 xl:pb-20  max-w-screen-xl">
+                <div class="relative max-w-screen-xl pt-10 ml-auto mr-auto xl:pt-24 xl:pb-20">
                     <h3 class="mb-3 text-lg font-bold text-center md:text-xl md:mb-6 xl:mb-8">Sledujte nás na sociálních sítích</h3>
                     <div>
                         <div class="flex flex-wrap items-center justify-center">
@@ -138,12 +97,70 @@
     </Layout>
 </template>
 
+<page-query>
+query {
+  craft {
+    entry(slug: "kontakt") {
+      ... on craft_contactPage_contactPage_Entry {
+        heading
+        excerpt
+        itemUrl
+        showContactButtons
+        subheading
+        body
+        contactFormHeader
+        threeTextColumns {
+          ... on craft_threeTextColumns_sloupce_BlockType {
+            id
+            header
+            description
+          }
+        }
+        map {
+          ...on craft_map_contactMap_BlockType {
+          	contactMapLink
+            contactMapImage {url}
+            contactMapNewWindow
+          }
+        }
+        contactAdress {
+        	...on craft_contactAdress_contactBlock_BlockType {
+            header
+            address
+            phone
+            email
+            description
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
 <script>
   import ContactForm from '../components/ContactForm'
+  import { mapObject } from  '~/components/utils';
+
   export default {
     components: {
       ContactForm
-    }
+    },
+    computed: {
+      page() {
+        return this.$page.craft.entry;
+      },
+    },
+    methods: {
+      mapObject,
+      getColumnSizeByOrder(index) {
+        const classNames = [
+          'w-full md:w-7/24 xl:w-6/24 md:pr-10 xl:pr-16',
+          'w-full md:w-10/24 xl:w-12/24 md:pr-10 xl:pr-16',
+          'w-full md:w-7/24 xl:w-6/24',
+        ];
+        return classNames[index];
+      }
+    },
   }
 </script>
 
