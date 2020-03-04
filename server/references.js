6556e1807c7b5a3a6fc977277b3cdc053a6e2d19
Jakub Robotka <jakub.robotka@gmail.com>
2020-03-04 08:53:13 +0100

    Fea: List fix url to detail and id for reference page

--- server/references.js
+++ server/references.js
@@ -4,7 +4,12 @@ async function referencePage(data, createPage) {
 
     const referenceUrl = slugifyUrlEntry(data.craft.referencePage.itemUrl, data.craft.referencePage.title);
     const categories = data.craft.categories;
-    createReferencePage(createPage, {url: referenceUrl, entry: {...data.craft.referencePage}, baseUrl: referenceUrl});
+    createReferencePage(createPage, {
+        url: referenceUrl,
+        entry: {...data.craft.referencePage},
+        baseUrl: referenceUrl,
+        referenceId: data.craft.referencePage.id
+    });
 
     categories.map(category => {
         createReferencePage(createPage, {
@@ -13,12 +18,13 @@ async function referencePage(data, createPage) {
                 id: category.id,
                 slug: data.craft.referencePage.slug,
             },
-            baseUrl: referenceUrl
+            baseUrl: referenceUrl,
+            referenceId: data.craft.referencePage.id
         });
     });
 
     data.craft.referenceRecords.map(referenceRecord => {
-        const referenceDetailUrl =  livePreviewEnabled
+        const referenceDetailUrl = livePreviewEnabled
             ? referenceUrl + slugifyUrlEntry(referenceRecord.itemUrl, referenceRecord.title)
             : referenceUrl + '/' + referenceRecord.slug;
 
@@ -33,7 +39,7 @@ async function referencePage(data, createPage) {
     });
 }
 
-function createReferencePage(createPage, {baseUrl, url, entry}) {
+function createReferencePage(createPage, {baseUrl, url, entry, referenceId}) {
     createPage({
         path: url,
         component: './src/templates/ReferencePage.vue',
@@ -41,7 +47,8 @@ function createReferencePage(createPage, {baseUrl, url, entry}) {
             id: entry.id,
             slug: entry.slug,
             services: baseUrl !== url ? entry.id : null,
-            baseUrl: baseUrl
+            baseUrl: baseUrl,
+            referenceId
         }
     });
 }
--- server/utils.js
+++ server/utils.js
@@ -1,9 +1,9 @@
 const slugify = require('@sindresorhus/slugify');
 
-const livePreviewEnabled = process.env.CRAFT_LIVE_PREVIEW;
+const livePreviewEnabled = process.env.GRIDSOME_CRAFT_GRAPHQL_ENDPOINT;
 
 function slugifyUrlEntry(itemUrl, title) {
-    return itemUrl || '/' + slugify(title);
+    return itemUrl !== "" ?  itemUrl : '/' + slugify(title);
 }
 
 module.exports = {
