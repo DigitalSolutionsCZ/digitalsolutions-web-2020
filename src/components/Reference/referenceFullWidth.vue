99613dcf5ce3c53bd6f68ced6d4df34172001eb1
Daniel Kalivoda <danek.kalivoda@gmail.com>
2020-03-12 19:29:23 +0100

    fixes

--- src/components/Reference/referenceFullWidth.vue
+++ src/components/Reference/referenceFullWidth.vue
@@ -3,18 +3,23 @@
         class="w-full mx-auto transition-all duration-200 ease-in-out bg-white shadow-none md:rounded group hover:shadow-xl md:w-22/24"
     >
         <div class="relative z-20 p-4 md:p-8 xl:pb-8 xl:px-16 xl:pt-16">
-            <g-link :to="props.baseUrl" class="block w-18/24 md:w-16/24">
+            <g-link
+                :to="props.baseUrl"
+                :class="props.reference.referenceMultipleImages.length > 0 ? 'w-18/24 md:w-16/24' : 'w-full'"
+                class="block"
+            >
                 <h2 class="mb-2 text-base font-black text-black xl:mb-3 xl:leading-tight md:text-xl xl:text-2xl">
                     {{ props.reference.heading }}
                 </h2>
             </g-link>
             <span
-                class="inline-block mb-4 text-sm text-green-500 md:mb-5 md:mb-8"
+                class="inline-block mb-4 text-sm text-green-500"
                 v-if="parent.mapObject(props.reference, ['vyberKlienta', 0, 'title'])"
             >
                 {{ parent.mapObject(props.reference, ['vyberKlienta', 0, 'title']) }}
             </span>
-            <div class="w-full text-xs leading-relaxed text-gray-900 md:text-base md:w-12/24 xl:w-14/24"
+            <div class="w-full text-xs leading-relaxed text-gray-900 md:text-base "
+                 :class="props.reference.referenceMultipleImages.length > 0 ? 'md:w-12/24 xl:w-14/24' : 'w-full'"
                  v-html="props.reference.excerpt"/>
         </div>
         <sub-link
@@ -24,7 +29,7 @@
         <div
             class="bottom-0 right-0 z-20 grid grid-cols-11 gap-4 px-2 text-right pointer-events-none md:absolute md:w-full md:grid-cols-none backface-visibility-hidden translate-z-0">
             <div
-                class="self-end col-start-2 col-end-7 row-start-1 row-end-1 pointer-events-auto md:col-start-8 md:col-end-11 translate-z-0"
+                class="self-end col-start-2 col-end-7 row-start-1 row-end-1 pointer-events-auto md:col-start-8 md:col-end-11 translate-z-0 z-10 relative"
                 v-if="props.reference.referenceMultipleImages.length > 0">
                 <g-link
                     :to="props.baseUrl"
