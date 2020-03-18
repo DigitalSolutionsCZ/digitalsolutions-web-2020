ff9307852dcb2a6e06ad43b23bf0be8620c012c7
Jan Dusek <jan.dusek@digitalcz.cz>
2020-03-18 15:51:31 +0100

    Add custom breakpoint for h ero slide

--- src/components/Layouts/Header.vue
+++ src/components/Layouts/Header.vue
@@ -1,59 +1,59 @@
 <template>
     <header
-        class="fixed inset-x-0 top-0 shadow-lg md:shadow-none"
+        class="fixed inset-x-0 top-0 shadow-lg mobileLandscape:shadow-none"
         :class="[white ? '' : 'bg-white']"
     >
         <div class="relative">
-            <div class="flex justify-between mx-auto max-w-screen-3xl h-13 md:h-19">
+            <div class="flex justify-between mx-auto max-w-screen-3xl h-13 mobileLandscape:h-19">
                 <div class="flex pl-4">
                     <slot name="logo">
-                        <g-link to="/" class="relative flex items-center w-36 md:w-48">
+                        <g-link to="/" class="relative flex items-center w-36 mobileLandscape:w-48">
                             <g-image src="../../images/logo-ds.png" alt="logo" class="w-auto pl-2"/>
                         </g-link>
                     </slot>
                 </div>
                 <div
-                    class="absolute inset-x-0 z-20 items-center justify-center flex-grow xs:left-auto xs:w-full top-full md:flex md:relative md:top-auto xs:max-w-xs md:visible md:max-w-none md:items-stretch"
+                    class="absolute inset-x-0 z-20 items-center justify-center flex-grow xs:left-auto xs:w-full top-full mobileLandscape:flex mobileLandscape:relative mobileLandscape:top-auto xs:max-w-xs mobileLandscape:visible mobileLandscape:max-w-none mobileLandscape:items-stretch"
                     :class="{'invisible': !open}"
                 >
                     <nav
-                        class="flex flex-col justify-center w-full py-2 text-base font-medium origin-top-right transform bg-white rounded shadow-lg menu-transition md:flex-row md:shadow-none md:bg-transparent md:py-0 md:transform-none md:opacity-100 md:transition-none"
+                        class="flex flex-col justify-center w-full py-2 text-base font-medium origin-top-right transform bg-white rounded shadow-lg menu-transition mobileLandscape:flex-row mobileLandscape:shadow-none mobileLandscape:bg-transparent mobileLandscape:py-0 mobileLandscape:transform-none mobileLandscape:opacity-100 mobileLandscape:transition-none"
                         :class="[white ? 'cha-nav' : 'text-gray-400', open ? 'translate-y-0 translate-x-0' : 'scale-50 translate-x-4 -translate-y-8 opacity-0']"
                     >
                         <template v-for="menuItem in menu">
                             <g-link
                                 :key="menuItem.id"
-                                class="relative flex p-4 text-xs transition-all duration-200 ease-in-out md:py-0 md:mr-3 xl:mr-10 group lg:text-base focus:outline-none"
-                                :class="[white ? 'cha-nav-link md:cha-nav-link-color' : 'hover:text-gray-900', {'text-gray-900': activeMenu(menuItem)}]"
+                                class="relative flex p-4 text-xs transition-all duration-200 ease-in-out mobileLandscape:py-0 mobileLandscape:mr-3 xl:mr-10 group lg:text-base focus:outline-none"
+                                :class="[white ? 'cha-nav-link mobileLandscape:cha-nav-link-color' : 'hover:text-gray-900', {'text-gray-900': activeMenu(menuItem)}]"
                                 :to="getUrl(mapObject(menuItem, ['menuLink', '0', 'itemUrl']), mapObject(menuItem, ['menuLink', '0', 'title']), mapObject(menuItem, ['menuLink', '0', 'slug']))"
                             >
-                                <div class="leading-tight md:relative md:flex">
+                                <div class="leading-tight mobileLandscape:relative mobileLandscape:flex">
                                     <span class="self-center">
                                         {{ menuItem.menuName }}
                                     </span>
                                     <div
-                                        class="absolute inset-y-0 left-0 w-1 my-1 transition-all duration-200 ease-in-out rounded-r opacity-0 bg-gradient-r-blue-green md:transform md:-translate-y-1/2 md:rounded md:mt-0 md:top-full md:bottom-auto md:inset-x-0 md:h-1 group-hover:opacity-100 md:w-auto"
-                                        :class="[white ? 'md:cha-nav-link-underline': 'bg-gradient-r-blue-green', {'opacity-100': activeMenu(menuItem)}]"
+                                        class="absolute inset-y-0 left-0 w-1 my-1 transition-all duration-200 ease-in-out rounded-r opacity-0 bg-gradient-r-blue-green mobileLandscape:transform mobileLandscape:-translate-y-1/2 mobileLandscape:rounded mobileLandscape:mt-0 mobileLandscape:top-full mobileLandscape:bottom-auto mobileLandscape:inset-x-0 mobileLandscape:h-1 group-hover:opacity-100 mobileLandscape:w-auto"
+                                        :class="[white ? 'mobileLandscape:cha-nav-link-underline': 'bg-gradient-r-blue-green', {'opacity-100': activeMenu(menuItem)}]"
                                     />
                                 </div>
                             </g-link>
                         </template>
                     </nav>
                 </div>
-                <div class="flex items-center md:pr-3">
+                <div class="flex items-center mobileLandscape:pr-3">
                     <div class="flex items-center justify-end">
                         <g-link
-                            class="inline-flex justify-center px-4 py-2 text-xs font-semibold leading-tight transition-all duration-200 ease-in-out rounded-full md:px-5 md:py-3 md:text-sm"
-                            :class="[white ? 'cha-nav-button' : 'text-green-500 border border-gray-100 md:border-none md:text-white md:bg-gradient-r-blue-green active']"
+                            class="inline-flex justify-center px-4 py-2 text-xs font-semibold leading-tight transition-all duration-200 ease-in-out rounded-full mobileLandscape:px-5 mobileLandscape:py-3 mobileLandscape:text-sm"
+                            :class="[white ? 'cha-nav-button' : 'text-green-500 border border-gray-100 mobileLandscape:border-none mobileLandscape:text-white mobileLandscape:bg-gradient-r-blue-green active']"
                             :to="getUrl(mapObject(contact, ['linkUrl', '0', 'itemUrl']), mapObject(contact, ['linkUrl', '0', 'slug']))"
                         >
                             {{ mapObject(contact, ['linkTitle']) }}
                         </g-link>
-                        <div class="relative z-50 inline-block p-3 mx-2 cursor-pointer md:hidden focus:outline-none" @click.prevent="toggle" tabindex="1" data-toggle-menu>
+                        <div class="relative z-50 inline-block p-3 mx-2 cursor-pointer mobileLandscape:hidden focus:outline-none" @click.prevent="toggle" tabindex="1" data-toggle-menu>
                             <div class="w-5 h-4 hamburger" :class="{'is-active': open}">
                                 <div
                                     class="bg-gray-600 hamburger-inner h-2px"
-                                    :class="{'md:bg-white': white}"
+                                    :class="{'ls:bg-white': white}"
                                 />
                             </div>
                         </div>
